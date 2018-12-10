33a17d36b4ef2b9ea66b3f55c1696a356d17a7cf
zc <13718051965@163.com>
2018-12-10 17:02:40 +0800

    ···

---
+++ BMMusicPlayer/ZCHUD.framework/Headers/TestHud.h
@@ -0,0 +1,17 @@
+//
+//  TestHud.h
+//  ZCHUD
+//
+//  Created by 张葱 on 2018/12/10.
+//  Copyright © 2018年 聪哥. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestHud : NSObject
++(void)show;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BMMusicPlayer/ZCHUD.framework/Headers/ZCHUD.h
@@ -0,0 +1,19 @@
+//
+//  ZCHUD.h
+//  ZCHUD
+//
+//  Created by 张葱 on 2018/12/10.
+//  Copyright © 2018年 聪哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZCHUD.
+FOUNDATION_EXPORT double ZCHUDVersionNumber;
+
+//! Project version string for ZCHUD.
+FOUNDATION_EXPORT const unsigned char ZCHUDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZCHUD/PublicHeader.h>
+
+#import "TestHud.h"
